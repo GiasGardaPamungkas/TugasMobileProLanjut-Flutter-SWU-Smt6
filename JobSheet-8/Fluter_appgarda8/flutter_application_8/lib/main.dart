512374c5ea13392b0141c29b3e7bd7bbab5f7fbe
GiasBelajar <giasgardapamungkas@gmail.com>
2024-06-24 15:33:51 +0700

    Tugas

--- JobSheet-8/Fluter_appgarda8/flutter_application_8/lib/main.dart
+++ JobSheet-8/Fluter_appgarda8/flutter_application_8/lib/main.dart
@@ -10,12 +10,12 @@ void main() {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                BuatKotak(Colors.greenAccent, 100, 'Halo'),
-                BuatKotak(Colors.orangeAccent[400]!, 70, 'Saya'),
-                BuatKotak(Colors.greenAccent, 50, 'Gias'),
-                BuatKotak(Colors.orangeAccent[400]!, 90, 'Garda'),
-                BuatKotak(Colors.red[300]!, 110, 'Pamungkas'),
-                BuatKotak(Colors.blue[300]!, 30, 'eh'),
+                buatKotak(Colors.greenAccent, 100, 'Halo'),
+                buatKotak(Colors.orangeAccent[400]!, 70, 'Saya'),
+                buatKotak(Colors.greenAccent, 50, 'Gias'),
+                buatKotak(Colors.orangeAccent[400]!, 90, 'Garda'),
+                buatKotak(Colors.red[300]!, 110, 'Pamungkas'),
+                buatKotak(Colors.blue[300]!, 30, 'EH'),
               ],
             ),
           ),
@@ -25,51 +25,16 @@ void main() {
   ));
 }
 
-class BuatKotak extends StatefulWidget {
-  final Color warna;
-  final double ukuran;
-  final String nama;
-
-  BuatKotak(this.warna, this.ukuran, this.nama);
-
-  @override
-  _BuatKotakState createState() => _BuatKotakState();
-}
-
-class _BuatKotakState extends State<BuatKotak> {
-  bool _isTextVisible = false;
-
-  void _toggleTextVisibility() {
-    setState(() {
-      _isTextVisible = !_isTextVisible;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _toggleTextVisibility,
-      child: Container(
-        height: widget.ukuran,
-        width: widget.ukuran,
+Widget buatKotak(Color warna, double ukuran, String nama) {
+  return Column(
+    children: [
+      Container(
+        decoration: BoxDecoration(color: warna),
+        height: ukuran,
+        width: ukuran,
         margin: const EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          color: widget.warna,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Center(
-          child: _isTextVisible
-              ? Text(
-                  widget.nama,
-                  style: TextStyle(
-                    color: const Color.fromARGB(255, 0, 0, 0),
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                )
-              : null,
-        ),
       ),
-    );
-  }
+      Text(nama),
+    ],
+  );
 }
